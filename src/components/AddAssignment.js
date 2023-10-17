9fdeda832ab36939650f68115b8f4a0483c6c0e1
Conlyn Pattison <cpattison@csumb.edu>
2023-10-17 11:19:09 -0700

    Revert "Dev - REST Implementation on Frontend"

--- src/components/AddAssignment.js
+++ src/components/AddAssignment.js
@@ -1,116 +1,13 @@
-import React, { useState } from "react";
-import { SERVER_URL } from "../constants";
+import React, { useState } from 'react';
 
-const AddAssignment = (props) => {
-  const [assignmentName, setAssignmentName] = useState("");
-  const [dueDate, setDueDate] = useState("");
-  const [courseTitle, setCourseTitle] = useState("");
-  const [courseId, setCourseId] = useState(0);
-  const [message, setMessage] = useState("");
 
-  const headers = ["Name", "Due Date", "Course ID"];
+function AddAssignment(props) { 
 
-  const onChangeName = (e) => {
-    setMessage("");
-    setAssignmentName(e.target.value);
-  };
-
-  const onChangeDate = (e) => {
-    setMessage("");
-    setDueDate(e.target.value);
-  };
-
-  const onChangeId = (e) => {
-    setMessage("");
-    setCourseId(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    const dtoObject = {
-      assignmentName,
-      dueDate,
-      courseTitle: "",
-      courseId,
-    };
-
-    setMessage("");
-
-    fetch(`${SERVER_URL}/assignment`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(dtoObject),
-    })
-      .then(async (response) => {
-        if (response.ok) return response.text();
-        else {
-          const data = await response.json();
-          setMessage(data.message);
-          return;
-        }
-      })
-      .then(
-        (data) =>
-          data !== undefined &&
-          setMessage(
-            `Successfully created assignment with id: ${parseInt(data, 10)}`
-          )
-      );
-  };
 
   return (
-    <div>
-      <h3>Create Assignment</h3>
-      <div margin="auto">
-        <h4>{message}&nbsp;</h4>
-        <table className="Center">
-          <thead>
-            <tr>
-              {headers.map((title, idx) => (
-                <th key={idx}>{title}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <input
-                  name="name"
-                  value={assignmentName}
-                  type="text"
-                  placeholder="Assignment Name"
-                  onChange={(e) => onChangeName(e)}
-                />
-              </td>
-              <td>
-                <input
-                  name="date"
-                  value={dueDate}
-                  type="date"
-                  placeholder="YYYY-MM-DD"
-                  onChange={(e) => onChangeDate(e)}
-                />
-              </td>
-              <td>
-                <input
-                  name="id"
-                  value={courseId}
-                  type="number"
-                  placeholder="00000000"
-                  onChange={(e) => onChangeId(e)}
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <br />
-        <button id="sgrade" type="button" margin="auto" onClick={handleSubmit}>
-          Submit
-        </button>
+      <div>
       </div>
-    </div>
-  );
-};
+  ); 
+}
 
 export default AddAssignment;
--- src/components/ListAssignment.js
+++ src/components/ListAssignment.js
@@ -1,119 +1,61 @@
-import React, { useState, useEffect } from "react";
-import { SERVER_URL } from "../constants";
-import { Link } from "react-router-dom";
+import React, {useState, useEffect} from 'react';
+import {SERVER_URL} from '../constants';
+import {Link} from 'react-router-dom';
+
 
 function ListAssignment(props) {
+
   const [assignments, setAssignments] = useState([]);
-  const [message, setMessage] = useState("");
-  const [forceChecked, setForceChecked] = useState(false);
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    // called once after intial render
-    fetchAssignments();
-  }, []);
-
+   // called once after intial render
+   fetchAssignments();
+  }, [] )
+ 
   const fetchAssignments = () => {
     console.log("fetchAssignments");
-    setMessage("");
     fetch(`${SERVER_URL}/assignment`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("assignment length " + data.length);
-        setAssignments(data);
-      })
-      .catch((err) => console.error(err));
-  };
-
-  const handleToggleCheck = (e) => {
-    setMessage("");
-    setForceChecked(e.target.checked);
-  };
-
-  const handleDelete = (id) => {
-    setMessage("");
-    console.log("deleteAssignment " + id);
-    fetch(
-      `${SERVER_URL}/assignment/${id}${forceChecked ? "?force=true" : ""}`,
-      {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
+    .then((response) => response.json() ) 
+    .then((data) => { 
+      console.log("assignment length "+data.length);
+      setAssignments(data);
+     }) 
+    .catch(err => console.error(err)); 
+  }
+  
+  
+    const headers = ['Assignment Name', 'Course Title', 'Due Date', ' ', ' ', ' '];
+    
+    return (
+      <div>
+        <h3>Assignments</h3>
+        <div margin="auto" >
+          <h4>{message}&nbsp;</h4>
+              <table className="Center"> 
+                <thead>
+                  <tr>
+                    {headers.map((title, idx) => (<th key={idx}>{title}</th>))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {assignments.map((row, idx) => (
+                    <tr key={idx}>
+                      <td>{row.assignmentName}</td>
+                      <td>{row.courseTitle}</td>
+                      <td>{row.dueDate}</td>
+                      <td>
+                        <Link to={`/gradeAssignment/${assignments[idx].id}`} >Grade</Link>
+                      </td>
+                      <td>Edit</td>
+                      <td>Delete</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+          </div>
+      </div>
     )
-      .then(async (response) => {
-        if (response.ok) {
-          setAssignments(
-            [...assignments].filter((assignment) => assignment.id !== id)
-          );
-          setMessage("Assignment successfully deleted");
-        } else {
-          const data = await response.json();
-          setMessage(data.message);
-        }
-      })
-      .catch((err) => console.error(err));
-  };
-
-  const headers = [
-    "Assignment Name",
-    "Course Title",
-    "Due Date",
-    " ",
-    " ",
-    " ",
-  ];
-
-  return (
-    <div>
-      <h3>Assignments</h3>
-      <div margin="auto">
-        <h4>{message}&nbsp;</h4>
-        <table className="Center">
-          <thead>
-            <tr>
-              {headers.map((title, idx) => (
-                <th key={idx}>{title}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {assignments.map((row, idx) => (
-              <tr key={idx}>
-                <td>{row.assignmentName}</td>
-                <td>{row.courseTitle}</td>
-                <td>{row.dueDate}</td>
-                <td>
-                  <Link to={`/gradeAssignment/${assignments[idx].id}`}>
-                    Grade
-                  </Link>
-                </td>
-                <td>
-                  <Link to={`/editAssignment/${assignments[idx].id}`}>
-                    Edit
-                  </Link>
-                </td>
-                <td>
-                  <button onClick={() => handleDelete(row.id)}>Delete</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <br />
-        <Link to={`/addAssignment`}> Add Assignment </Link>
-      </div>
-      <div className="right container">
-        <label htmlFor="force"> Force Delete? </label>
-        <input
-          id="force"
-          type="checkbox"
-          value={forceChecked}
-          onChange={(e) => handleToggleCheck(e)}
-        ></input>
-      </div>
-    </div>
-  );
-}
+}  
 
 export default ListAssignment;
